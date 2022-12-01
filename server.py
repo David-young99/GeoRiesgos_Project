9fdb1b903e817c34a7781e243863b1c9d9035e66
unknown <davidyflol@gmail.com>
2022-12-01 00:03:53 +0000

    asd

---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template, jsonify
+
+datos = [{}]
+
+app = Flask(__name__)
+
+@app.route("/")
+def template():
+    return render_template("form.html")
+
+@app.route("/form", methods = ["POST"])
+def form():
+        name = request.form["name"]
+        email = request.form["email"]
+        alert = request.form["alert"]
+        details = request.form["details"]
+
+        global datos
+        datos = [
+    {
+        "name": name
+    },
+    {
+        "email": email
+    },
+    {
+        "alert": alert
+    },
+    {
+        "details": details
+    }
+                ]
+
+        return render_template("form_final.html")
+
+
+@app.route("/json/bridge")
+def json():
+    
+   return jsonify(datos)
+
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
